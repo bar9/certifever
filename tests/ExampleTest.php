bc1011a8b1afd31050aaa73b360349463f82ede6
rb <r.brand@iwf.ch>
2023-01-11 17:51:55 +0100

    add phpunit

---
+++ tests/ExampleTest.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Tests;
+
+use PHPUnit\Framework\TestCase;
+
+class ExampleTest extends TestCase
+{
+    public function testRequest() {
+        self::assertTrue(true);
+    }
+}
